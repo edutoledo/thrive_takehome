1bbe1aa8f8bacd3db78591b905aa40657f0b6ddd
Eduardo Toledo <edu.betti@gmail.com>
2023-09-14 12:56:25 -0400

    [GREEN] parse users passes specs

--- challenge.rb
+++ challenge.rb
@@ -1,5 +1,15 @@
 # frozen_string_literal: true
 
-def parse_users(_users)
-  nil
+require 'pry'
+
+User = Struct.new(:id, :first_name, :last_name, :email, :company_id, :email_status, :active_status, :tokens)
+
+def parse_users(users)
+  users.map do |user|
+    # binding.pry
+    User.new(
+      user['id'], user['first_name'], user['last_name'], user['email'], user['company_id'],
+      user['email_status'], user['active_status'], user['tokens']
+    )
+  end
 end
